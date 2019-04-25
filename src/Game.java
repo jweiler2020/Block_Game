bff9be5eb5d9aea8d6f4aa28af19d4eda0084f41
jweiler2020 <jweiler2020@cusd.claremont.edu>
2019-04-25 07:46:52 -0700

    Implemented double buffering and delta time calculations

--- src/Game.java
+++ src/Game.java
@@ -6,38 +6,49 @@ import javax.swing.*;
 public class Game extends JFrame
 	implements ActionListener
 {
-	private int i = 0;
+	private Image backImage;
+	private static final int w = 800;
+	private static final int h = 600;
+	
+	private long timeStart;
+	
+	private double x, y;
 	
 	public Game()
 	{
 		super("Block Game 2019 Edition™");
 		
 		new Timer((int)(1000/60f), this).start();
+		timeStart = System.nanoTime();
 	}
 	
 	public void paint(Graphics g)
 	{
-		setBackground(Color.WHITE);
+		if(backImage == null || backImage.getWidth(null) != w || backImage.getHeight(null) != h)
+			backImage = createImage(w, h);
 		
-		int w = getWidth();
-		int h = getHeight();
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, w, h);
-		g.setColor(Color.BLACK);
+		Graphics imgG = backImage.getGraphics();
+		super.paint(imgG);
+		draw(imgG);
 		
-		g.fillRect(w/4 + i, h/4+i, w/2, h/2);
-		i++;
+		g.drawImage(backImage, 0, 0, null);
 	}
 	
-	public void actionPerformed(ActionEvent e)
+	private void draw(Graphics g)
 	{
-		repaint();
+		double dt = (System.nanoTime() - timeStart)/1000000000f;
+		g.fillRect((int)x, (int)y, w/4, h/4);
+		x += 20*dt;
+		y += 20*dt;
+		timeStart = System.nanoTime();
 	}
 	
-	public static void main(String[] args)
+	public void actionPerformed(ActionEvent e) { repaint(); }
+	
+	public static void main(String[] options)
 	{
 		Game win = new Game();
-		win.setBounds(200, 200, 800, 600);
+		win.setBounds(200, 200, w, h);
 		win.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		win.setResizable(false);
 		win.setVisible(true);
